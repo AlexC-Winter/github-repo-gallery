3964ae00abc3e1bd6284c572108ad49b15257245
Alex_W <alexcwinter7@gmail.com>
2023-11-27 17:08:07 -0600

    GitHub Profile API fetch added

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 const overviewDiv = document.querySelector(".overview");
 console.log(overviewDiv);
+// The div for the overview profile section
+
+const username = "AlexC-Winter"
+// GitHub usernamne
+
+const githubProfile = async function () {
+    const res = await fetch(
+        `https://api.github.com/users/${username}`
+    )
+    const data = await res.json()
+    console.log(data)
+}
+
+githubProfile()
