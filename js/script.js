a582423dcf91ee5e144c2fe97afb4aa7fe32e3ac
Alex_W <alexcwinter7@gmail.com>
2023-11-27 18:39:23 -0600

    user data added

--- js/script.js
+++ js/script.js
@@ -10,7 +10,23 @@ const githubProfile = async function () {
         `https://api.github.com/users/${username}`
     )
     const data = await res.json()
-    console.log(data)
+    userDisplay(data)
 }
 
 githubProfile()
+
+const userDisplay = function(data){
+    const div = document.createElement("div")
+    div.classList.add("user-info")
+    div.innerHTML= `
+        <figure>
+            <img alt="user avatar" src=${data.avatar_url} />
+        </figure>
+        <div>
+            <p><strong>Name:</strong> ${data.name}</p>
+            <p><strong>Bio:</strong> ${data.bio}</p>
+            <p><strong>Location:</strong> ${data.location}</p>
+            <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+        </div> `
+    overviewDiv.append(div)
+}
